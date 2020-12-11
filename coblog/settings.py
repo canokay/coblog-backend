5c05bc3fda71f203d54423910de52c7da41558bc
Can Okay <canokay2014@gmail.com>
2020-12-11 04:05:40 +0300

    0.4.1 - rest framework config changed.

--- coblog/settings.py
+++ coblog/settings.py
@@ -128,9 +128,8 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 MEDIA_URL = "/media/"
 
 REST_FRAMEWORK = {
-    # Use Django's standard `django.contrib.auth` permissions,
-    # or allow read-only access for unauthenticated users.
-    'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
     ]
 }
