e5cc6163dd126275e5356aaee0ad405000b032b4
Can Okay <canokay2014@gmail.com>
2020-12-11 04:05:48 +0300

    0.4.2 - BlogListView and BlogDetailView created.

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from blog.models import Blog
+from blog.serializers import BlogListSerializer, BlogDetailSerializer
+from rest_framework import viewsets
+from rest_framework.views  import APIView
+from rest_framework.generics import ListAPIView
+from rest_framework.response import Response
 
-# Create your views here.
+"""
+class BlogListView(APIView):
+    def get(self, request, format=None):
+        blogs = Blog.objects.all()
+        serializer = BlogListSerializer(blogs, many=True)
+        return Response(serializer.data)
+"""
+
+class BlogListView(ListAPIView):
+    serializer_class = BlogListSerializer
+
+    def get_queryset(self):
+        return Blog.objects.all()
+
+
+
+class BlogDetailView(APIView):
+    def get(self, request, *args, **kwargs):
+        blog = Blog.objects.filter(id=self.kwargs['id'])
+        serializer = BlogDetailSerializer(blog, many=True)
+        return Response(serializer.data)
