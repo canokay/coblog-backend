ea29e6cfc2cd1291af4b553cbf354109b872c13d
Can Okay <canokay2014@gmail.com>
2020-12-14 04:40:45 +0300

    0.4.5 - CORS_ORIGIN_WHITELIST react url added.

--- coblog/settings.py
+++ coblog/settings.py
@@ -138,5 +138,6 @@ REST_FRAMEWORK = {
 
 CORS_ORIGIN_ALLOW_ALL = False
 CORS_ORIGIN_WHITELIST = (
-    'http://localhost:8080',
+    'http://localhost:8080',#Vuejs
+    'http://localhost:3000'#Reactjs
 )
